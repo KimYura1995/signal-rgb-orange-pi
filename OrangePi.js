ce2dfb2dcc9dea3de446eedc7d94ba38537bf685
y.kim@qsoft.ru <y.kim@qsoft.ru>
2025-04-20 22:47:44 +0300

    test

--- OrangePi.js
+++ OrangePi.js
@@ -125,8 +125,8 @@ class WLCDevice {
 		for(let CurrPosX = 0; CurrPosX < 2; CurrPosX++) {
 			let color = device.color(CurrPosX, 0);
 			dataArray = [
-				CurrPosX === 0 ? 1 : 0,
-				CurrPosX === 1 ? 1 : 0,
+				CurrPosX === 0 ? 255 : 0,
+				CurrPosX === 1 ? 255 : 0,
 				color[0],
 				color[1],
 				color[2],
