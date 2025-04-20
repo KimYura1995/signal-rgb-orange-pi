1bcbd799207ecced828dee347be1fe4db41cb80f
y.kim@qsoft.ru <y.kim@qsoft.ru>
2025-04-20 21:52:49 +0300

    test

--- OrangePi.js
+++ OrangePi.js
@@ -130,16 +130,25 @@ export function Initialize() {
 }
 
 export function Render() {
-	device.log(device.color(0, 0));
-	WLC.SendColorPackets();
-
-	if(HWLupdateRequested == true) {
-		const currentTime = Date.now();
-
-		if(currentTime - lastHWLchange >= 3000) {
-			WLC.updateHWLsettings();
-		}
+	let packet = [];
+	for (let i = 0, i < 2; i++) {
+		packet[] = device.color(i, 0);
+		//Buffer.from(packet.flat());
 	}
+	device.log(packet);
+
+	//udp.send(this.ip, this.streamingPort, packet, BIG_ENDIAN);
+
+
+	//WLC.SendColorPackets();
+
+	//if(HWLupdateRequested == true) {
+		//const currentTime = Date.now();
+
+		//if(currentTime - lastHWLchange >= 3000) {
+		//	WLC.updateHWLsettings();
+		//}
+	//}
 }
 
 export function onStatusLED_enableChanged(){ HWL_setting_updated(); }
